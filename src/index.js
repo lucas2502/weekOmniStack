4670c4bb5b613b54eb464a62cb40f7b83b212262
lucas2502 <lucas2502@live.com>
2019-02-08 14:38:43 -0200

    Maybe finish backand aplication ;)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);//real time
 
 mongoose.connect("mongodb://goweek:goweek123@ds133231.mlab.com:33231/weekomnistack", {
     usNewParser: true    
 });
 
+app.use((req, res, next) =>{
+    req.io = io;
+
+    return next();
+});
+
+app.use(cors());
 app.use(express.json());//Inform all routes use json
 app.use(require('./routes'));
 
@@ -14,6 +25,6 @@ app.use(require('./routes'));
     return res.send('Hello Word')
 });*///Send menssage in window
 
-app.listen(3000, () => {
+server.listen(3000, () => {
     console.log('server started on port : 3000')
 });

cfa0bd4f78c3f2b2f3ea033a7b14b55e1086d407
lucas2502 <lucas2502@live.com>
2019-06-21 16:30:03 -0300

    Create routes(post image, post like, get files)

--- back-end/src/controllers/PostController.js
+++ back-end/src/controllers/PostController.js
@@ -2,10 +2,23 @@ const Post = require('../models/Post')
 
 module.exports = {
     async index(req, res) {
+        const posts = await Post.find().sort('-createAt')
 
+        return res.json(posts)
     },
 
     async store(req, res) {
-        return res.json({ ok: true })
+        const { author, place, description, hashtags } = req.body
+        const { filename: image} = req.file
+
+        const post = await Post.create({
+            author,
+            place,
+            description,
+            hashtags,
+            image,
+        })
+
+        return res.json(post)
     }
 }

bb6eff45fa56f9b4903a573777d387dd1fa5f3c9
lucas2502 <lucas2502@live.com>
2019-06-25 17:19:42 -0300

    Finish create page feed

--- front-end/frontend/src/pages/Feed.js
+++ front-end/frontend/src/pages/Feed.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import api from '../services/api'
 
 import './Feed.css'
 
@@ -9,64 +10,50 @@ import send from '../assets/send.png'
 
 
 class Feed extends Component {
+    state = {
+        feed: [],
+    }
+    
+    async componentDidMount() {
+        const response = await api.get('posts')
+
+        this.setState({ feed: response.data })
+    }
+
     render() {
         return (
             <section id="post-list">
-                <article>
-                    <header>
-                        <div className="user-info">
-                            <span>User Name</span>
-                            <span className="place">Location</span>
-                        </div>
+                {
+                    this.state.feed.map( post => (
+                        <article>
+                            <header>
+                                <div className="user-info">
+                                    <span>{post.author}</span>
+                                    <span className="place">{post.place}</span>
+                                </div>
 
-                        <img className="icon" src={more} alt="Mais"/>
-                    </header>
-                    <img src="http://localhost:3333/files/image.png" alt="" />
+                                <img className="icon" src={more} alt="Mais"/>
+                            </header>
+                            <img src={`http://localhost:3333/files/{post.image}`} alt="" />
 
-                    <footer>
-                        <div className="actions">
-                            <img className="icon" src={like} alt=""/>
-                            <img className="icon" src={comment} alt=""/>
-                            <img className="icon" src={send} alt=""/>
+                            <footer>
+                                <div className="actions">
+                                    <img className="icon" src={like} alt=""/>
+                                    <img className="icon" src={comment} alt=""/>
+                                    <img className="icon" src={send} alt=""/>
 
-                            <strong>9000 curtidas</strong>
+                                    <strong>{post.likes}</strong>
 
-                            <p>
-                                Um post muito massa!
-                                <span>#react #top</span>
-                            </p>
+                                    <p>
+                                        {post.description}
+                                        <span>{post.hashtags}</span>
+                                    </p>
 
-                        </div>
-                    </footer>
-                </article>
-
-                <article>
-                    <header>
-                        <div className="user-info">
-                            <span>User Name</span>
-                            <span className="place">Location</span>
-                        </div>
-
-                        <img className="icon" src={more} alt="Mais"/>
-                    </header>
-                    <img src="http://localhost:3333/files/image.png" alt="" />
-
-                    <footer>
-                        <div className="actions">
-                            <img className="icon" src={like} alt=""/>
-                            <img className="icon" src={comment} alt=""/>
-                            <img className="icon" src={send} alt=""/>
-
-                            <strong>900 curtidas</strong>
-
-                            <p>
-                                Um post muito massa!
-                                <span>#react #top</span>
-                            </p>
-
-                        </div>
-                    </footer>
-                </article>
+                                </div>
+                            </footer>
+                        </article>
+                    ))
+                }
             </section>
         )
     }
